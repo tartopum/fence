25afe848d3468a38cfe60e60033f76a75db4ad96
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-12-25 20:34:58 +0100

    Remove unused code

--- server.py
+++ server.py
@@ -3,15 +3,3 @@ if __name__ == '__main__':
     addr = ('', 8000)
     httpd = http.server.HTTPServer(addr, http.server.CGIHTTPRequestHandler)
     httpd.serve_forever()
-"""
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('home.html')
-
-if __name__ == '__main__':
-    app.run()
-"""
