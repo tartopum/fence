dd7069ef3be03876d570575f10900051a868d84d
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-02-27 15:53:17 +0100

    Add routes to command lights

--- arduino/fence/fence.ino
+++ arduino/fence/fence.ino
@@ -4,7 +4,7 @@
 #include <WebServer.h> // https://github.com/sirleech/Webduino
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ip(192, 168, 167, 100);
+IPAddress ip(192, 168, 168, 115);
 WebServer webserver("", 80);
 
 int STOP_OUTPUT = CONTROLLINO_RELAY_03;
@@ -32,17 +32,30 @@ int light1State = LOW;
 int light2State = LOW;
 
 
-void home(WebServer &server, WebServer::ConnectionType type, char *, bool) {
-  server.httpSuccess();
-  if (type == WebServer::GET) {
-    P(msg) = "https://github.com/tartopum/fence";
-    server.printP(msg);
-  }
+void switchOutput(int pin) {
+    if(digitalRead(pin) == HIGH) {
+        digitalWrite(pin, LOW);
+    } else {
+        digitalWrite(pin, HIGH);
+    }
 }
 
-void fenceState(WebServer &server, WebServer::ConnectionType type, char *, bool) {
-  server.httpSuccess();
-  if (type == WebServer::GET) {
+void homeRoute(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    server.httpSuccess();
+    if (type == WebServer::GET) {
+        P(msg) = "https://github.com/tartopum/fence";
+        server.printP(msg);
+    }
+}
+
+void fenceRoute(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    server.httpSuccess();
+
+    if (type == WebServer::POST) {
+        switchOutput(FENCE_OUTPUT);
+        switchOutput(FENCE_LED_OUTPUT);
+    }
+
     if (digitalRead(FENCE_OUTPUT)) {
         P(msg) = "0";
         server.printP(msg);
@@ -50,94 +63,120 @@ void fenceState(WebServer &server, WebServer::ConnectionType type, char *, bool)
         P(msg) = "1";
         server.printP(msg);
     }
-  }
 }
 
-void fenceOn(WebServer &server, WebServer::ConnectionType type, char *, bool) {
-  server.httpSuccess();
-  digitalWrite(FENCE_LED_OUTPUT, LOW);
-  digitalWrite(FENCE_OUTPUT, LOW);
+void lightIn1Route(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    server.httpSuccess();
+    
+    if (type == WebServer::POST) {
+        switchOutput(LIGHT_IN1_OUTPUT);
+    }
+
+    if (digitalRead(LIGHT_IN1_OUTPUT)) {
+        P(msg) = "1";
+        server.printP(msg);
+    } else {
+        P(msg) = "0";
+        server.printP(msg);
+    }
 }
 
-void fenceOff(WebServer &server, WebServer::ConnectionType type, char *, bool) {
-  server.httpSuccess();
-  digitalWrite(FENCE_LED_OUTPUT, HIGH);
-  digitalWrite(FENCE_OUTPUT, HIGH);
+void lightIn2Route(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    server.httpSuccess();
+    
+    if (type == WebServer::POST) {
+        switchOutput(LIGHT_IN2_OUTPUT);
+    }
+
+    if (digitalRead(LIGHT_IN2_OUTPUT)) {
+        P(msg) = "1";
+        server.printP(msg);
+    } else {
+        P(msg) = "0";
+        server.printP(msg);
+    }
 }
 
-void switchRelay(int pin) {
-  if(digitalRead(pin) == HIGH) {
-    digitalWrite(pin, LOW);
-  } else {
-    digitalWrite(pin, HIGH);
-  }
+void lightOutRoute(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    server.httpSuccess();
+    
+    if (type == WebServer::POST) {
+        switchOutput(LIGHT_OUT_OUTPUT);
+    }
+
+    if (digitalRead(LIGHT_OUT_OUTPUT)) {
+        P(msg) = "1";
+        server.printP(msg);
+    } else {
+        P(msg) = "0";
+        server.printP(msg);
+    }
 }
 
 void light() {
-  int light1 = digitalRead(LIGHT1_INPUT);
-  int light2 = digitalRead(LIGHT2_INPUT);
-  unsigned long mill = millis();
+    int light1 = digitalRead(LIGHT1_INPUT);
+    int light2 = digitalRead(LIGHT2_INPUT);
+    unsigned long mill = millis();
 
-  if(mill - lightLastDebounceTime <= lightDebounceDelay) {
-    return;
-  }
-  lightLastDebounceTime = mill;
+    if(mill - lightLastDebounceTime <= lightDebounceDelay) {
+        return;
+    }
+    lightLastDebounceTime = mill;
 
-  if(light1State == LOW && light1 == HIGH) {
-    light1LastHigh = mill;
-    light1Active = true;
-  }
-  if(light2State == LOW && light2 == HIGH) {
-    light2LastHigh = mill;
-    light2Active = true;
-  }
+    if(light1State == LOW && light1 == HIGH) {
+        light1LastHigh = mill;
+        light1Active = true;
+    }
+    if(light2State == LOW && light2 == HIGH) {
+        light2LastHigh = mill;
+        light2Active = true;
+    }
 
-  if(light1 == HIGH && light2 == HIGH) { // Outdoor
-    if (mill - light1LastHigh > LIGHT_PRESS_DELAY && mill - light2LastHigh > LIGHT_PRESS_DELAY && light1Active && light2Active) {
-      switchRelay(LIGHT_OUT_OUTPUT); 
-      light1Active = false;
-      light2Active = false;
+    if(light1 == HIGH && light2 == HIGH) { // Outdoor
+        if (mill - light1LastHigh > LIGHT_PRESS_DELAY && mill - light2LastHigh > LIGHT_PRESS_DELAY && light1Active && light2Active) {
+            switchOutput(LIGHT_OUT_OUTPUT); 
+            light1Active = false;
+            light2Active = false;
+        }
+    } else {
+        if(light1 == HIGH && light1Active && mill - light1LastHigh > LIGHT_PRESS_DELAY) {
+            switchOutput(LIGHT_IN1_OUTPUT); 
+            light1Active = false;
+        }
+        if(light2 == HIGH && light2Active && mill - light2LastHigh > LIGHT_PRESS_DELAY) {
+            switchOutput(LIGHT_IN2_OUTPUT); 
+            light2Active = false;
+        }
     }
-  } else {
-    if(light1 == HIGH && light1Active && mill - light1LastHigh > LIGHT_PRESS_DELAY) {
-      switchRelay(LIGHT_IN1_OUTPUT); 
-      light1Active = false;
-    }
-    if(light2 == HIGH && light2Active && mill - light2LastHigh > LIGHT_PRESS_DELAY) {
-      switchRelay(LIGHT_IN2_OUTPUT); 
-      light2Active = false;
-    }
-  }
-  
-  light1State = light1;
-  light2State = light2;
+    
+    light1State = light1;
+    light2State = light2;
 }
 
 void setup() {
-  pinMode(FENCE_OUTPUT, OUTPUT);
+    pinMode(FENCE_OUTPUT, OUTPUT);
+    pinMode(FENCE_LED_OUTPUT, OUTPUT);
+    pinMode(LIGHT1_INPUT, INPUT);
+    pinMode(LIGHT2_INPUT, INPUT);
+    pinMode(LIGHT_IN1_OUTPUT, OUTPUT);
+    pinMode(LIGHT_IN2_OUTPUT, OUTPUT);
+    pinMode(LIGHT_OUT_OUTPUT, OUTPUT);
 
-  pinMode(LIGHT1_INPUT, INPUT);
-  pinMode(LIGHT2_INPUT, INPUT);
-  pinMode(LIGHT_IN1_OUTPUT, OUTPUT);
-  pinMode(LIGHT_IN2_OUTPUT, OUTPUT);
-  pinMode(LIGHT_OUT_OUTPUT, OUTPUT);
-
-  light1State = digitalRead(LIGHT_IN1_OUTPUT);
-  light2State = digitalRead(LIGHT_IN2_OUTPUT);
-  
-  Ethernet.begin(mac, ip);
-  webserver.setDefaultCommand(&home);
-  webserver.addCommand("fence/on", &fenceOn);
-  webserver.addCommand("fence/off", &fenceOff);
-  webserver.addCommand("fence/state", &fenceState);
-  webserver.begin();
+    light1State = digitalRead(LIGHT_IN1_OUTPUT);
+    light2State = digitalRead(LIGHT_IN2_OUTPUT);
+    
+    Ethernet.begin(mac, ip);
+    webserver.setDefaultCommand(&homeRoute);
+    webserver.addCommand("fence", &fenceRoute);
+    webserver.addCommand("light_in1", &lightIn1Route);
+    webserver.addCommand("light_in2", &lightIn2Route);
+    webserver.addCommand("light_out", &lightOutRoute);
+    webserver.begin();
 }
 
 void loop() {
-  /*
-  char buff[64];
-  int len = 64;
-  webserver.processConnection(buff, &len);
-  */
-  light(); 
+    char buff[64];
+    int len = 64;
+    webserver.processConnection(buff, &len);
+    light(); 
 }
